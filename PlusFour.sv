8a2ba161dfa13647f9092bcdb5a52249b791ab95
GiselleMylHin <167721565+GiselleMylHin@users.noreply.github.com>
2025-03-14 16:49:14 -0700

    Update PlusFour.sv

--- PlusFour.sv
+++ PlusFour.sv
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Cal Poly
+// Engineer: Giselle Hinahon
 // 
-// Create Date: 03/11/2025 10:38:06 PM
+// Create Date: 03/01/2025 05:18:52 PM
 // Design Name: 
 // Module Name: PC
 // Project Name: 
@@ -21,10 +21,8 @@
 
 
 module PlusFour(
-// declaring inputs and outputs
-     input logic [0:31] SUM_IN,
+     input logic [0:31] SUM_IN,                // declaring inputs and outputs
      output logic [0:31] SUM_OUT
      );
-     //adds 4 to the imput value adn outputs it
-     assign SUM_OUT = 4'b0100+SUM_IN;
+     assign SUM_OUT = 4'b0100+SUM_IN;          //adds 4 to the input value and outputs the sum
 endmodule
